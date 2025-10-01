542e9d161f2d4bfbc3d922ba1890f82d1a4a34a9
Monde Sineke <nsineke@gmail.com>
2025-10-01 15:30:51 +0200

    fix: tailwind not working

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,14 +1,6 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs))
 }
-
-export function getBaseURL() {
-  return process.env.NODE_ENV === "production"
-    ? process.env.NEXT_PUBLIC_BASE_URL
-    : "http://localhost:3000";
-}
-
-export const isProduction = process.env.NODE_ENV === "production";
